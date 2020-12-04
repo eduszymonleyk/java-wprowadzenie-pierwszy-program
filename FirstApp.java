f2d615970399b95b05ab089342831e833ba6ead0
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-04 15:05:23 +0100

    pierwszy program w javie

---
+++ FirstApp.java
@@ -0,0 +1,25 @@
+class FirstApp { 
+	public static void main(String args[]){
+		// pierwszy program
+		System.out.println("Dzisiaj jest piêkna pogoda.\n");
+	
+
+		// zadanie 1
+		System.out.println("Pan pomidor wlaz³ na tyczkê");
+		System.out.println("I przedrzeŸnia ogrodniczkê.");
+		System.out.println("\"Jak pan mo¿e,");
+		System.out.println("Panie pomidorze?!\"");
+
+		System.out.println();
+
+		// zadanie 2
+		System.out.println("------------------------------");
+		System.out.println("Do biedronki przyszed³ ¿uk,");
+		System.out.println("W okieneczko puk-puk-puk.");
+		System.out.println();
+		System.out.println("Panieneczka widzi ¿uka:");
+		System.out.println("\"Czego pan tu u mnie szuka?\"");
+		System.out.println("------------------------------");
+
+	}
+}
